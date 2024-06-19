f0017a93ab90763efce3c38853520945c3d9aac8
Eldoov <zuowent@bu.edu>
2024-06-19 15:33:38 -0400

    update

--- Web101/results-summary-component-main/scirpt.js
+++ Web101/results-summary-component-main/scirpt.js
@@ -1,25 +1,36 @@
+/*
 var score = document.createElement('div');
 score.className = 'socre';
 var stats = document.createElement('div');
 stats.className = 'stats';
 var span = document.createElement('span');
+*/
 
-/*
 fetch("data.json").then(response =>
     response.json()
 ).then(json => 
-    console.log(json)
+    showData(json)
 );
-*/
+
+
 
 function showData(data) {
+    let res = 0;
     for (i = 0; i < data.length; i++) {
         // document.getElementById("stats"+i).appendChild('stats');
+
+        var img = document.createElement('img');
 
         let category = data[i].category;
         let s = date[i].score;
         let icon = data[i].icon;
 
+        img.src = icon
+        document.getElementsByClassName('stats')[i].appendChild(img);
+        document.getElementsByClassName('stats')[i].textContent = category;
+        document.getElementsByClassName('score')[i].textContent = s;
+
+        res += s;
     }
 
 }
