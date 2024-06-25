edb3765ef685e2b4f16771237147bafc53e49c9f
Eldoov <zuowent@bu.edu>
2024-06-24 20:10:39 -0400

    update

--- Web101/interactive-comments-section-main/script.js
+++ Web101/interactive-comments-section-main/script.js
@@ -1,31 +1,54 @@
-let postInfo = [];
+const replyBtn = document.querySelectorAll('reply');
 
 fetch("data.json").then(response =>
     response.json()
 ).then(data =>
-    processData(data)
+    processData(data.comments)
 );
 
-function comments(postID, postContent, timeStamp, score, username) {
+function comments(postID, postContent, timeStamp, score, avatar, username) {
     this.postID = postID;
     this.postContent = postContent;
     this.timeStamp = timeStamp;
     this.score = score;
+    this.avatar = avatar;
     this.username = username;
 }
 
 function processData(data){
-    postInfo = data.comments[0];
-    let comment = new comments(postInfo.id, postInfo.content, postInfo.createdAt, postInfo.score, postInfo.user); 
-    showComments(comment);
+    let postInfo = [];
+    for (let i = 0; i < data.length; i++){
+        postInfo = data[i];
+        let comment = new comments(postInfo.id, postInfo.content, postInfo.createdAt, postInfo.score, postInfo.user.image.png, postInfo.user.username);
+        showComments(comment);
+    }
 }
 
 function showComments(comment){
-    let temp, postBlock, postContent;
+    let temp, postBlock, postContent, user, avatar, time, score;
     var mainElement = document.querySelector('main');
+
     temp = document.getElementById("post-template");
-    postBlock = temp.content.querySelector(".post-block");
-    postContent = temp.content.querySelector(".post-content");
+    let clon = temp.content.cloneNode(true);
+
+    postBlock = clon.querySelector(".post");
+    user = clon.querySelector(".username");
+    avatar = clon.querySelector(".avatar");
+    time = clon.querySelector(".time-stamps");
+    score = clon.querySelector(".rating-score");
+    postContent = clon.querySelector(".post-content");
+
+    user.textContent = comment.username;
+    avatar.src = comment.avatar;
+    time.textContent = comment.timeStamp;
+    score.textContent = comment.score;
     postContent.textContent = comment.postContent;
     mainElement.appendChild(postBlock);
 }
+
+
+replyBtn.forEach((replyBtn) => {
+    replyBtn.addEventListener("click", e => {
+
+    })
+})
