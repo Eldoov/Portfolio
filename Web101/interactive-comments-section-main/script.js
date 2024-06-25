4d96348761b4060b14d3a259d6abd371747a4e31
Eldoov <zuowent@bu.edu>
2024-06-25 18:11:31 -0400

    update

--- Web101/interactive-comments-section-main/script.js
+++ Web101/interactive-comments-section-main/script.js
@@ -4,7 +4,7 @@
 fetch("data.json").then(response =>
     response.json()
 ).then(data =>
-    processData(data.comments)
+    getPost(data.comments)
 );
 
 function comments(postID, postContent, timeStamp, score, avatar, username) {
@@ -16,57 +16,83 @@ function comments(postID, postContent, timeStamp, score, avatar, username) {
     this.username = username;
 }
 
-function processData(data){
-    let postInfo = [];
+function getPost(data) {
+    let replies = [];
     for (let i = 0; i < data.length; i++){
-        postInfo = data[i];
-        let comment = new comments(postInfo.id, postInfo.content, postInfo.createdAt, postInfo.score, postInfo.user.image.png, postInfo.user.username);
-        showComments(comment);
+        replies = data[i].replies;
+        comment = createPost(data[i]);
+        showPost(comment, "");
+        if (replies.length != 0){
+            for(let j = 0; j < replies.length; j++){
+                reply = createPost(replies[j]);
+                showPost(reply, "replyID"+comment.postID);
+            }
+        } 
     }
     getReply();
 }
 
-function showComments(comment){
-    let temp, post, postContent, user, avatar, time, score, reply;
-    var mainElement = document.querySelector('main');
 
-    temp = document.getElementById("post-template");
+function createPost(postInfo){
+    let comment = new comments(postInfo.id, postInfo.content, postInfo.createdAt, postInfo.score, postInfo.user.image.png, postInfo.user.username);
+    return comment;
+}
+
+function showPost(comment, repID){
+    let temp, post, postContent, user, avatar, time, score, reply, mainElement;
+    let replyID, scoreID, inputID, postID;
+    let replyBtnID;
+
+    if (repID == ""){
+        temp = document.getElementById("post-template");
+        mainElement = document.querySelector('main');
+    }else {
+        temp = document.getElementById("post-template");
+        mainElement = document.getElementById(repID);
+    }
+
     let clon = temp.content.cloneNode(true);
 
-    post = clon.querySelector(".post");
+    post = clon.querySelector(".getBlock");
     user = clon.querySelector(".username");
     avatar = clon.querySelector(".avatar");
     time = clon.querySelector(".time-stamps");
     score = clon.querySelector(".rating-score");
     postContent = clon.querySelector(".post-content");
-    reply =  clon.querySelector(".input-block")
+    reply =  clon.querySelector(".input-block");
+
+    scoreID = clon.getElementById("scoreID");
+    replyID = clon.getElementById("replyID");
+    inputID = clon.getElementById("inputID");
+    postID = clon.getElementById("postID");
+    replyBtnID = clon.getElementById("replyBtnID");
+    
 
     user.textContent = comment.username;
     avatar.src = comment.avatar;
     time.textContent = comment.timeStamp;
     score.textContent = comment.score;
     postContent.textContent = comment.postContent;
+
+    postID.setAttribute("id", "postID"+comment.postID);
+    replyID.setAttribute("id", "replyID"+comment.postID);
+    scoreID.setAttribute("id", "scoreID"+comment.postID);
+    inputID.setAttribute("id", "inputID"+comment.postID);
+    replyBtnID.setAttribute("id", "replyBtnID"+comment.postID);
+
     mainElement.appendChild(post);
-    mainElement.appendChild(reply);
+    //mainElement.appendChild(reply);
 }
 
 function getReply() {
     const replyBtns = document.querySelectorAll('.reply-btn');
-    const replyBlock = document.getElementsByClassName('input-block');
-    let tmp = -1;
-    let index;
+    const replyBlock = document.querySelectorAll('.input-block');
+    
     replyBtns.forEach((btn) => {
-        btn.addEventListener('click', (event) => {
-            index = Array.prototype.indexOf.call(replyBtns, event.currentTarget);
-            console.log(index, tmp); 
-            if (index == tmp){
-                replyBlock.item(index).classList.add("hidden");
-                tmp = -1;
-            }else {
-                event.currentTarget.classList.add("selectre");
-                replyBlock.item(index).classList.remove("hidden");
-                tmp = index;
-            }
+        btn.addEventListener('click', e => {
+            let x = e.currentTarget.id.replace(/^\D+/g, '');
+            console.log(x);
+            replyBlock.item(x-1).classList.remove("hidden");
         });
     });
 }
